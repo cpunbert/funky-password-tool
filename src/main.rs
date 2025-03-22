1c60f485c68604813d8fda0a271ee5989ec9c625
cpunbert <hubert.lisica@gmail.com>
2025-03-22 19:28:34 +0100

    removed not working code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Error;
+use std::{f32::consts::E, io::Error, num::ParseIntError};
 
 use rand::Rng; /*should be replaced in the future */
 static PASSWORD_CHAR_SET: &str =
@@ -21,22 +21,19 @@ fn main() {
     one.display();
     two.display();
 }
-//? ditch enum, instead match 3rd arg as stren,
-//parse command line args one by one into variables???? and do stuff afterwards
-fn pinp(mut input: Vec<String>) -> Result<Command, Error> {
-    let command = input[0].to_lowercase().as_str();
-    let arg0 = input.remove(1).parse::<usize>();
-    Ok(Command::Display(vec![input[0]], (arg0, arg0)))
-}
+/*fn parse_usize(str: String) -> Result<usize, ParseIntError> {
+    let arg = str.parse::<usize>()?;
+    Ok(arg)
+}*/
+// parse input with .expect() just to make it work
 fn parse_input(mut input: Vec<String>) -> Result<Command, ()> {
     match input[0].to_lowercase().as_str() {
-        "show" => Ok(Command::Display),
+        "show" => Ok(Command::Display()),
         "new" => Ok(Command::New(input)),
         "edit" => Ok(Command::Edit(input)),
         _ => Err(()),
     }
 }
-//TODO: FIX function above
 
 fn generate_password(password_strength: PasswordStrength, password_length: usize) -> String {
     let mut rng = rand::rng();
@@ -93,7 +90,7 @@ enum PasswordStrength {
 }
 
 enum Command {
-    Display(Vec<String>, (usize, usize)),
+    Display(),
     New(Vec<String>),
     Edit(Vec<String>),
 }
