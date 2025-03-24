a01d9298dd05880bd156941937068cfbd10bfdde
cpunbert <hubert.lisica@gmail.com>
2025-03-24 22:42:07 +0100

    added feature:generate pasentry for command and pass it into entrylist

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
-use std::{f32::consts::E, fs::read, io::Error, num::ParseIntError};
-
 use rand::Rng; /*should be replaced in the future */
 static PASSWORD_CHAR_SET: &str =
     "abcdefghijklmnopqrstuwvxyzABCDEFGHIJKLMNOPQRSTUWVXYZ0123456789~`!@#$%^&*()-_+={}[]|;:<>,./?";
 fn main() {
     let mut input: Vec<String> = std::env::args().skip(1).collect();
-    for argument in &input {
-        println!("{argument}")
-    }
-    for argument in &input {
-        println!("{argument}")
-    }
+    let mut password_list = Vec::<PasswordEntry>::new();
     let one = PasswordEntry::new(
         String::from("google"),
         String::from("adam.nowak@gmail.com"),
         PasswordStrength::SpecialCharacters,
         40,
     );
-    one.display();
-    match PasswordEntry::new2(input) {
-        Ok(entry) => entry.display(),
-        Err(_) => println!("couldnt display entry"),
-    }
+    let asd = new2(input, password_list)?;
 }
-/*fn parse_usize(str: String) -> Result<usize, ParseIntError> {
-    let arg = str.parse::<usize>()?;
-    Ok(arg)
-}*/
-// parse input with .expect() just to make it work
+
 fn parse_input(mut input: Vec<String>) -> Result<Command, ()> {
     match input[0].to_lowercase().as_str() {
         "show" => Ok(Command::Show()),
@@ -61,6 +46,29 @@ struct PasswordEntry {
     password: String,
 }
 
+fn new2(mut input: Vec<String>, mut list: Vec<PasswordEntry>) -> Result<Vec<PasswordEntry>, ()> {
+    let name = input.remove(0);
+    let login = input.remove(0);
+    let pas_str = match input.remove(0).as_str() {
+        "1" => PasswordStrength::Lowercase,
+        "2" => PasswordStrength::Uppercase,
+        "3" => PasswordStrength::Numbers,
+        "4" => PasswordStrength::SpecialCharacters,
+        _ => return Err(()),
+    };
+    let pas_len = match input.remove(0).parse::<usize>() {
+        Ok(len) => len,
+        Err(_) => return Err(()),
+    };
+    let result = PasswordEntry {
+        name,
+        login,
+        password: generate_password(pas_str, pas_len),
+    };
+    list.push(result);
+    Ok(list)
+}
+
 impl PasswordEntry {
     fn new(name: String, login: String, pas_str: PasswordStrength, pas_len: usize) -> Self {
         PasswordEntry {
@@ -68,27 +76,6 @@ impl PasswordEntry {
             login: login,
             password: generate_password(pas_str, pas_len),
         }
-    }
-    fn new2(mut input: Vec<String>) -> Result<Self, ()> {
-        let name = input.remove(0);
-        let login = input.remove(0);
-        let pas_str = match input.remove(0).as_str() {
-            "1" => PasswordStrength::Lowercase,
-            "2" => PasswordStrength::Uppercase,
-            "3" => PasswordStrength::Numbers,
-            "4" => PasswordStrength::SpecialCharacters,
-            _ => return Err(()),
-        };
-        let pas_len = match input.remove(0).parse::<usize>() {
-            Ok(len) => len,
-            Err(_) => return Err(()),
-        };
-        let result = PasswordEntry {
-            name: name,
-            login: login,
-            password: generate_password(pas_str, pas_len),
-        };
-        Ok(result)
     }
 
     fn display(&self) {
